5d51ce0fa9c9c2ed43f9f51df3bb38caf439384d
Sam Freiberg <sam.freiberg@skybox.com>
2013-10-30 16:58:47 -0700

    Switching to map[string]string because github.com/go-sql-driver/mysql doesn't play nicely with interfaces.

--- dbutils.go
+++ dbutils.go
@@ -1,21 +1,24 @@
 package dbutils
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type InterfaceScanner struct {
 	Name  string
-	Value interface{}
+	Value string
 }
 
 // InterfaceScanner implements the sql.Scan interface
 func (s *InterfaceScanner) Scan(src interface{}) error {
-	s.Value = src
+	s.Value = fmt.Sprintf("%s", src)
 	return nil
 }
 
 // Convert a row to a map. Expects rows.Next() to have been already called.
-func ConvertRowToMap(r *sql.Rows) (map[string]interface{}, error) {
-	result := make(map[string]interface{})
+func ConvertRowToMap(r *sql.Rows) (map[string]string, error) {
+	result := make(map[string]string)
 
 	cols, err := r.Columns()
 	if err != nil {
