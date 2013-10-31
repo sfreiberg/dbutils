ba85a5c86f701d11c6b8e051513dfe5eefa2184f
Sam Freiberg <sam.freiberg@skybox.com>
2013-10-30 18:53:38 -0700

    Last commit broke postgres. This release finally works on postgres and mysql.

--- dbutils.go
+++ dbutils.go
@@ -7,18 +7,24 @@ import (
 
 type InterfaceScanner struct {
 	Name  string
-	Value string
+	Value interface{}
 }
 
 // InterfaceScanner implements the sql.Scan interface
 func (s *InterfaceScanner) Scan(src interface{}) error {
-	s.Value = fmt.Sprintf("%s", src)
+	switch src.(type) {
+	default:
+		s.Value = src
+	case []uint8:
+		s.Value = fmt.Sprintf("%s", src)
+	}
+
 	return nil
 }
 
 // Convert a row to a map. Expects rows.Next() to have been already called.
-func ConvertRowToMap(r *sql.Rows) (map[string]string, error) {
-	result := make(map[string]string)
+func ConvertRowToMap(r *sql.Rows) (map[string]interface{}, error) {
+	result := make(map[string]interface{})
 
 	cols, err := r.Columns()
 	if err != nil {
